21dac254d3be77174ce605bbfd1b196b64fc2ade
Sourashis Pal <palsourashis04@gmail.com>
2025-08-16 19:49:14 +0530

    added

---
+++ Chapter_4/Chapter_4-PS/Problem3.py
@@ -0,0 +1,7 @@
+a = (34, 249, "Soura")
+
+a[3] = "Loura"
+
+print(a)
+
+# It's simple means that tuple are im-mutable(can't be changed)
---
+++ Chapter_4/Chapter_4-PS/Problem5.py
@@ -0,0 +1,6 @@
+num = []
+
+for i in range(0,4):
+    num.append(int(input("Enter the numbers: ")))
+
+print("The sum of the numbers are:",sum(num))
