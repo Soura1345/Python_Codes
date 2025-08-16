0a88cacca89d5491e79eb57a9e309702e53bce3c
Sourashis Pal <palsourashis04@gmail.com>
2025-08-16 13:18:11 +0530

    Chapter 4

---
+++ Chapter_4/1_list.py
@@ -0,0 +1,8 @@
+friends = ["Apple", "Orange", 5, 3.14, False, "Akash", "Rohan"]
+
+print(friends[0])
+friends[0] = "Grapes" # Unlike str, list are mutable(can be changed)
+
+print(friends[0])
+
+print(friends[1:4])
---
+++ Chapter_4/2_List_methods.py
@@ -0,0 +1,9 @@
+friends = ["Apple", "Orange", 5, 3.14, False, "Akash", "Rohan"]
+# print(friends)
+
+friends.append("Soura")
+# print(friends)
+
+l1 = [34,21,75,1,53,13]
+l1.sort()
+print(l1)
