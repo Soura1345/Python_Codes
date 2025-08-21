a2826408666ea82013ce8644101922c21c1cfb34
Sourashis Pal <palsourashis04@gmail.com>
2025-08-21 17:14:17 +0530

    if, ELse, Elif statement

---
+++ Chapter_6/2_If_Elif_ELse_Ladder.py
@@ -0,0 +1,14 @@
+age = int(input("Enter your age: "))
+
+# If Elif Else Ladder
+
+if(age >= 18):
+    print("You are adult.")
+elif(age < 0):
+    print("Are you mad.")
+elif(age == 0):
+    print("Enter a valid age.")
+else:
+    print("You are child.")
+
+print("Good bye")
