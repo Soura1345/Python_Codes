dc21ac59124dffe240a032c4d453d15b40e8f8ff
Sourashis Pal <palsourashis04@gmail.com>
2025-08-22 16:28:18 +0530

    Fuction

---
+++ Chapter_8/1_Function.py
@@ -0,0 +1,11 @@
+# Function difination
+def avg():
+    a = int(input("enter first no.: "))
+    b = int(input("enter second no.: "))
+    c = int(input("enter third no.: ")) 
+
+    avg = a+b+c/3
+    print(avg)
+
+# Function Call
+avg()
