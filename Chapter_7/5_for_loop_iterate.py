2e3e16f9699c1afe3d34e45af125f0785feb398e
Sourashis Pal <palsourashis04@gmail.com>
2025-08-22 11:54:48 +0530

    for loop iterates

---
+++ Chapter_7/5_for_loop_iterate.py
@@ -0,0 +1,14 @@
+# FOR LOOP FOR LIST
+l = [1, "Harry", False, "Soura", "Snigdha", "Suman", "Soumen"]
+for i in l:
+    print(i)
+
+# FOR LOOP FOR TUPLE
+t = (1,2,3,454)
+for i in t:
+    print(i)
+
+# FOR LOOP FOR STRING
+s = "Soura"
+for i in s:
+    print(i)
