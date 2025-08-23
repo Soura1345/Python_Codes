7ea93d6d2bf4bea0457c9798c44d341b66b5e61b
Sourashis Pal <palsourashis04@gmail.com>
2025-08-23 19:47:44 +0530

    update

--- Chapter_9/5_with.py
+++ Chapter_9/5_with.py
@@ -1,10 +1,10 @@
-# f = open("Chapter_9/file.txt")
+# f = open("Text/file.txt")
 # print(f.read())
 # f.close()
 
 # The same can be written by 'with' statement
 
-with open("Chapter_9/file.txt") as f:
+with open("Text/file.txt") as f:
     print(f.read())
 
 # You don't have to close the file in this case
